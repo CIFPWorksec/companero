e835c92e1bdac751851f1310c1fdc22a0a46d747
dam <dam@203PC11>
2018-02-19 08:50:44 +0100

    Segunda versión 2

--- src/proyectocompañero/ProyectoCompañero.java
+++ src/proyectocompañero/ProyectoCompañero.java
@@ -16,8 +16,6 @@ public class ProyectoCompañero {
      */
     public static void main(String[] args) {
        System.out.println("hola compañero");
-       System.out.println("voy a hacer el primer cambio");
-       System.out.println("estoy en programacion");
-    }
+           }
     
 }
